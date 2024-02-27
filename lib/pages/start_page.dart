da09a47f6ae7945bb24649fe04b2c85d3a69297a
Marcin Kawa <lerukcja2002@gmail.com>
2024-02-27 19:48:21 +0100

    added photo background

--- lib/pages/start_page.dart
+++ lib/pages/start_page.dart
@@ -18,65 +18,78 @@ class _StartPageState extends State<StartPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('New Game'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextField(
-              controller: clubNameController,
-              decoration: InputDecoration(
-                labelText: 'Club Name',
-                errorText: showError
-                    ? 'Please enter a club name'
-                    : (showLengthError
-                        ? 'Club name is too long (max 20 characters)'
-                        : (showSpecialCharacterError
-                            ? 'Club name cannot contain special characters'
-                            : null)),
+      body: Stack(
+        children: [
+          // Background image
+          Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/start_game_bg.png'),
+                fit: BoxFit.cover,
               ),
             ),
-            const SizedBox(height: 10),
-            ElevatedButton(
-              onPressed: () async {
-                String clubName = clubNameController.text.trim();
+          ),
+          // Content
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextField(
+                  controller: clubNameController,
+                  decoration: InputDecoration(
+                    labelText: 'Club Name',
+                    errorText: showError
+                        ? 'Please enter a club name'
+                        : (showLengthError
+                            ? 'Club name is too long (max 20 characters)'
+                            : (showSpecialCharacterError
+                                ? 'Club name cannot contain special characters'
+                                : null)),
+                  ),
+                ),
+                const SizedBox(height: 10),
+                ElevatedButton(
+                  onPressed: () async {
+                    String clubName = clubNameController.text.trim();
 
-                // Check if the club name contains special characters
-                RegExp regex = RegExp(r'[!@#%^&*(),.?":{}|<>]');
-                if (regex.hasMatch(clubName)) {
-                  setState(() {
-                    showSpecialCharacterError = true;
-                  });
-                  return;
-                }
+                    // Check if the club name contains special characters
+                    RegExp regex = RegExp(r'[!@#%^&*(),.?":{}|<>]');
+                    if (regex.hasMatch(clubName)) {
+                      setState(() {
+                        showSpecialCharacterError = true;
+                      });
+                      return;
+                    }
 
-                if (clubName.isNotEmpty) {
-                  if (clubName.length <= 20) {
-                    await DatabaseHelper.instance
-                        .addNewClub(clubName, createdByPlayer: true);
+                    if (clubName.isNotEmpty) {
+                      if (clubName.length <= 20) {
+                        await DatabaseHelper.instance
+                            .addNewClub(clubName, createdByPlayer: true);
 
-                    if (!context.mounted) return;
+                        if (!context.mounted) return;
 
-                    Navigator.of(context).pushReplacement(MaterialPageRoute(
-                      builder: (context) => const HomePage(),
-                    ));
-                  } else {
-                    setState(() {
-                      showLengthError = true;
-                    });
-                  }
-                } else {
-                  setState(() {
-                    showError = true;
-                  });
-                }
-              },
-              child: const Text('OK'),
+                        Navigator.of(context).pushReplacement(
+                          MaterialPageRoute(
+                            builder: (context) => const HomePage(),
+                          ),
+                        );
+                      } else {
+                        setState(() {
+                          showLengthError = true;
+                        });
+                      }
+                    } else {
+                      setState(() {
+                        showError = true;
+                      });
+                    }
+                  },
+                  child: const Text('OK'),
+                ),
+              ],
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
