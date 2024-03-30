59c4831b86702b2012fa977e2ef0b75b4082cd6e
Marcin Kawa <lerukcja2002@gmail.com>
2024-03-30 21:04:51 +0100

    add firebase project

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'package:pocket_eleven/pages/main_menu.dart';
 import 'image_loader.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   runApp(Builder(
     builder: (context) {
