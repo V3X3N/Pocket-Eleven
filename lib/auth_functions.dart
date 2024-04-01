874242639eeae1b112ccde9224c7e7354bb213d0
Marcin Kawa <lerukcja2002@gmail.com>
2024-04-01 18:27:00 +0200

    email login/register functionality

---
+++ lib/auth_functions.dart
@@ -0,0 +1,57 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:pocket_eleven/pages/home_page.dart';
+import 'package:pocket_eleven/pages/login_page.dart';
+import 'package:pocket_eleven/firebase_options.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:pocket_eleven/firebase_functions.dart';
+
+import 'image_loader.dart';
+
+class AuthServices {
+  static signupUser(
+      String email, String password, String name, BuildContext context) async {
+    try {
+      UserCredential userCredential = await FirebaseAuth.instance
+          .createUserWithEmailAndPassword(email: email, password: password);
+
+      await FirebaseAuth.instance.currentUser!.updateDisplayName(name);
+      await FirebaseAuth.instance.currentUser!.verifyBeforeUpdateEmail(email);
+      await FirestoreFunctions.saveUser(name, email, userCredential.user!.uid);
+      ScaffoldMessenger.of(context)
+          .showSnackBar(SnackBar(content: Text('Registration Successful')));
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
+        ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text('Password Provided is too weak')));
+      } else if (e.code == 'email-already-in-use') {
+        ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text('Email Provided already Exists')));
+      }
+    } catch (e) {
+      ScaffoldMessenger.of(context)
+          .showSnackBar(SnackBar(content: Text(e.toString())));
+    }
+  }
+
+  static signinUser(String email, String password, BuildContext context) async {
+    try {
+      await FirebaseAuth.instance
+          .signInWithEmailAndPassword(email: email, password: password);
+
+      ScaffoldMessenger.of(context)
+          .showSnackBar(SnackBar(content: Text('You are Logged in')));
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
+        ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text('No user Found with this Email')));
+      } else if (e.code == 'wrong-password') {
+        ScaffoldMessenger.of(context)
+            .showSnackBar(SnackBar(content: Text('Password did not match')));
+      }
+    }
+  }
+}
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,5 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:pocket_eleven/auth_functions.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:pocket_eleven/pages/home_page.dart';
+import 'package:pocket_eleven/pages/login_page.dart';
+import 'package:pocket_eleven/firebase_options.dart';
+import 'package:pocket_eleven/image_loader.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -11,6 +18,12 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   bool _isLoading = true;
   late Image _loadingImage;
+  final _formKey = GlobalKey<FormState>();
+  String email = '';
+  String password = '';
+  bool login = false;
+
+  late String fullname;
 
   @override
   void initState() {
@@ -29,100 +42,111 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomInset: false,
-      body: Stack(
-        children: [
-          _isLoading
-              ? const Center(
-                  child: CircularProgressIndicator(),
-                )
-              : Container(
-                  decoration: BoxDecoration(
-                    image: DecorationImage(
-                      image: _loadingImage.image,
-                      fit: BoxFit.cover,
+      appBar: AppBar(
+        elevation: 0,
+        title: Text('Login'),
+      ),
+      body: Form(
+        key: _formKey,
+        child: Container(
+          padding: EdgeInsets.all(14),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // ======== Full Name ========
+              login
+                  ? Container()
+                  : TextFormField(
+                      key: const ValueKey('fullname'),
+                      decoration: const InputDecoration(
+                        hintText: 'Enter Full Name',
+                      ),
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return 'Please Enter Full Name';
+                        } else {
+                          return null;
+                        }
+                      },
+                      onSaved: (value) {
+                        setState(() {
+                          fullname = value!;
+                        });
+                      },
                     ),
-                  ),
+
+              // ======== Email ========
+              TextFormField(
+                key: ValueKey('email'),
+                decoration: InputDecoration(
+                  hintText: 'Enter Email',
                 ),
-          Positioned(
-            top: MediaQuery.of(context).size.height * 0.25,
-            left: 0,
-            right: 0,
-            child: const Center(
-              child: Column(
-                children: [
-                  Text(
-                    'POCKET',
-                    style: TextStyle(
-                      fontSize: 44.0,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
-                    ),
-                  ),
-                  Text(
-                    'ELEVEN',
-                    style: TextStyle(
-                      fontSize: 44.0,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
-                    ),
-                  ),
-                ],
+                validator: (value) {
+                  if (value!.isEmpty || !value.contains('@')) {
+                    return 'Please Enter valid Email';
+                  } else {
+                    return null;
+                  }
+                },
+                onSaved: (value) {
+                  setState(() {
+                    email = value!;
+                  });
+                },
               ),
-            ),
-          ),
-          Positioned(
-            bottom: MediaQuery.of(context).size.height * 0.3,
-            left: 0,
-            right: 0,
-            child: Center(
-              child: Column(
-                children: [
-                  const TextField(
-                    decoration: InputDecoration(
-                      hintText: 'Email',
-                      filled: true,
-                      fillColor: Colors.white70,
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  const TextField(
-                    decoration: InputDecoration(
-                      hintText: 'Password',
-                      filled: true,
-                      fillColor: Colors.white70,
-                    ),
-                    obscureText: true,
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  MaterialButton(
-                    height: 40,
-                    minWidth: 100,
-                    color: Colors.blueAccent,
-                    onPressed: () {
-                      // TODO: Implement Account Login process
-                      Navigator.pushAndRemoveUntil(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => const HomePage(),
-                        ),
-                        (route) => false,
-                      );
+              // ======== Password ========
+              TextFormField(
+                key: ValueKey('password'),
+                obscureText: true,
+                decoration: InputDecoration(
+                  hintText: 'Enter Password',
+                ),
+                validator: (value) {
+                  if (value!.length < 6) {
+                    return 'Please Enter Password of min length 6';
+                  } else {
+                    return null;
+                  }
+                },
+                onSaved: (value) {
+                  setState(() {
+                    password = value!;
+                  });
+                },
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Container(
+                height: 55,
+                width: double.infinity,
+                child: ElevatedButton(
+                    onPressed: () async {
+                      if (_formKey.currentState!.validate()) {
+                        _formKey.currentState!.save();
+                        login
+                            ? AuthServices.signinUser(email, password, context)
+                            : AuthServices.signupUser(
+                                email, password, fullname, context);
+                      }
                     },
-                    child: const Text(
-                      "Confirm",
-                      style: TextStyle(color: Colors.white),
-                    ),
-                  ),
-                ],
+                    child: Text(login ? 'Login' : 'Signup')),
               ),
-            ),
+              SizedBox(
+                height: 10,
+              ),
+              TextButton(
+                  onPressed: () {
+                    setState(() {
+                      login = !login;
+                    });
+                  },
+                  child: Text(login
+                      ? "Don't have an account? Signup"
+                      : "Already have an account? Login"))
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
