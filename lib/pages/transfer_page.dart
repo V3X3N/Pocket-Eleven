5bca8cf767202b416c75f3580bfafdeb6759bf20
Marcin Kawa <lerukcja2002@gmail.com>
2024-05-03 20:22:22 +0200

    add Shared_Prefs and name generation functionality

--- lib/pages/transfer_page.dart
+++ lib/pages/transfer_page.dart
@@ -1,38 +1,116 @@
+import 'dart:async';
+import 'dart:convert';
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:pocket_eleven/design/colors.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class TransferPage extends StatefulWidget {
-  const TransferPage({super.key});
+  const TransferPage({Key? key}) : super(key: key);
 
   @override
   State<TransferPage> createState() => _TransferPageState();
 }
 
 class _TransferPageState extends State<TransferPage> {
+  List<String> footballers = [
+    'Cristiano',
+    'Lionel',
+    'Neymar',
+    'Kylian',
+    'Mohamed',
+    'Robert',
+    'Sergio',
+    'Kevin',
+    'Luka',
+    'Karim',
+    'Eden',
+    'Sadio',
+    'Virgil',
+    'Paulo',
+    'Harry',
+    'Raheem',
+    'N\'Golo',
+    'Manuel',
+    'Jan',
+    'Thiago',
+  ];
+
+  List<String> selectedFootballers = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _loadSelectedFootballers();
+  }
+
+  Future<void> _loadSelectedFootballers() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String? footballersJson = prefs.getString('selectedFootballers');
+    if (footballersJson != null) {
+      setState(() {
+        selectedFootballers = List<String>.from(jsonDecode(footballersJson));
+      });
+    }
+  }
+
+  Future<void> _generateRandomFootballers() async {
+    final _random = Random();
+    List<String> tempList = [];
+    for (int i = 0; i < 6; i++) {
+      int randomIndex = _random.nextInt(footballers.length);
+      String selectedFootballer = footballers[randomIndex];
+      tempList.add(selectedFootballer);
+    }
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setString('selectedFootballers', jsonEncode(tempList));
+    setState(() {
+      selectedFootballers = tempList;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColors.primaryColor,
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          SizedBox(height: 50),
+          SizedBox(height: 20),
+          Column(
+            children: selectedFootballers.map((name) {
+              return Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8.0),
+                child: Text(
+                  name,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                  ),
+                ),
+              );
+            }).toList(),
+          ),
+          Expanded(
+            child: Container(),
+          ),
           Container(
             margin: const EdgeInsets.symmetric(horizontal: 20.0),
             height: 80,
             child: MaterialButton(
               onPressed: () {
-                // TODO: Handle button press for PLAY
+                _generateRandomFootballers();
               },
               padding: const EdgeInsets.all(16.0),
-              color: AppColors.green,
+              color: Colors.orangeAccent,
               shape: RoundedRectangleBorder(
                 borderRadius: BorderRadius.circular(10.0),
               ),
               child: const Center(
                 child: Text(
-                  'PLAY',
+                  'DRAW',
                   style: TextStyle(
                     color: Colors.white,
                     fontSize: 24,
@@ -42,6 +120,7 @@ class _TransferPageState extends State<TransferPage> {
               ),
             ),
           ),
+          SizedBox(height: 50),
         ],
       ),
     );
