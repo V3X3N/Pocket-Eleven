60675ff9b2809c07c8cbc4e9fffe85003f9798b9
Marcin Kawa <lerukcja2002@gmail.com>
2024-05-03 19:49:42 +0200

    first players page set

--- lib/pages/transfer_page.dart
+++ lib/pages/transfer_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pocket_eleven/design/colors.dart';
 
 class TransferPage extends StatefulWidget {
   const TransferPage({super.key});
@@ -10,8 +11,43 @@ class TransferPage extends StatefulWidget {
 class _TransferPageState extends State<TransferPage> {
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("Transfer")
+    return Scaffold(
+      backgroundColor: AppColors.primaryColor,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        toolbarHeight: 1,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Container(
+            margin: const EdgeInsets.symmetric(horizontal: 20.0),
+            height: 80,
+            child: MaterialButton(
+              onPressed: () {
+                // TODO: Handle button press for PLAY
+              },
+              padding: const EdgeInsets.all(16.0),
+              color: AppColors.green,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              child: const Center(
+                child: Text(
+                  'PLAY',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
