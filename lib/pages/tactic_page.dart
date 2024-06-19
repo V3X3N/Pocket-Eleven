f7d84051cdb097263210b05f66b8ff4da8a51002
Marcin Kawa <lerukcja2002@gmail.com>
2024-06-19 11:48:05 +0200

    tactic page structure

--- lib/pages/tactic_page.dart
+++ lib/pages/tactic_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:pocket_eleven/design/colors.dart';
+import 'package:pocket_eleven/player.dart';
 
 class TacticPage extends StatefulWidget {
   const TacticPage({super.key});
@@ -11,11 +12,15 @@ class TacticPage extends StatefulWidget {
 class _TacticPageState extends State<TacticPage> {
   bool _isLoading = true;
   late Image _leagueImage;
+  List<Player> selectedFootballers = [];
+  List<Player?> fieldPositions = List.filled(11, null);
+  List<Player?> benchPlayers = List.filled(4, null);
 
   @override
   void initState() {
     super.initState();
     _loadLeagueImage();
+    _generateRandomFootballers();
   }
 
   void _loadLeagueImage() {
@@ -23,6 +28,17 @@ class _TacticPageState extends State<TacticPage> {
 
     setState(() {
       _isLoading = false;
+    });
+  }
+
+  Future<void> _generateRandomFootballers() async {
+    List<Player> tempList = [];
+    for (int i = 0; i < 15; i++) {
+      tempList.add(await Player.generateRandomFootballer());
+    }
+
+    setState(() {
+      selectedFootballers = tempList;
     });
   }
 
@@ -55,9 +71,106 @@ class _TacticPageState extends State<TacticPage> {
                       fit: BoxFit.cover,
                     ),
                   ),
+                  child: Column(
+                    children: [
+                      Expanded(
+                        child: GridView.builder(
+                          padding: const EdgeInsets.all(20),
+                          gridDelegate:
+                              const SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 3,
+                            childAspectRatio: 1,
+                            mainAxisSpacing: 10,
+                            crossAxisSpacing: 10,
+                          ),
+                          itemCount: fieldPositions.length,
+                          itemBuilder: (context, index) {
+                            return DragTarget<Player>(
+                              builder: (context, candidateData, rejectedData) {
+                                return Container(
+                                  decoration: BoxDecoration(
+                                    border: Border.all(
+                                      color: AppColors.textEnabledColor,
+                                    ),
+                                    borderRadius: BorderRadius.circular(8),
+                                  ),
+                                  child: fieldPositions[index] != null
+                                      ? Draggable<Player>(
+                                          data: fieldPositions[index],
+                                          feedback: _buildPlayerAvatar(
+                                              fieldPositions[index]!),
+                                          childWhenDragging: Container(),
+                                          child: _buildPlayerAvatar(
+                                              fieldPositions[index]!),
+                                        )
+                                      : Container(),
+                                );
+                              },
+                              onAccept: (data) {
+                                setState(() {
+                                  fieldPositions[index] = data;
+                                  selectedFootballers.remove(data);
+                                });
+                              },
+                            );
+                          },
+                        ),
+                      ),
+                      const Divider(
+                        color: AppColors.textEnabledColor,
+                        height: 1,
+                      ),
+                      Expanded(
+                        child: ListView.builder(
+                          padding: const EdgeInsets.all(20),
+                          scrollDirection: Axis.horizontal,
+                          itemCount: selectedFootballers.length,
+                          itemBuilder: (context, index) {
+                            Player player = selectedFootballers[index];
+                            return Draggable<Player>(
+                              data: player,
+                              feedback: _buildPlayerAvatar(player),
+                              childWhenDragging: Container(),
+                              child: _buildPlayerAvatar(player),
+                            );
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
         ],
       ),
+    );
+  }
+
+  Widget _buildPlayerAvatar(Player player) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Image.asset(
+          player.imagePath,
+          width: 50,
+          height: 50,
+        ),
+        const SizedBox(height: 4),
+        Text(
+          player.name,
+          style: const TextStyle(
+            color: AppColors.textEnabledColor,
+            fontSize: 12,
+          ),
+          textAlign: TextAlign.center,
+        ),
+        Text(
+          player.position,
+          style: const TextStyle(
+            color: AppColors.textEnabledColor,
+            fontSize: 12,
+          ),
+          textAlign: TextAlign.center,
+        ),
+      ],
     );
   }
 }
