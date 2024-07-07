2406956d0fbb98911ec400ecbe416f4ca18ec880
Marcin Kawa <lerukcja2002@gmail.com>
2024-07-07 19:02:07 +0200

    choosing between formations

--- lib/pages/tactic_page.dart
+++ lib/pages/tactic_page.dart
@@ -12,23 +12,59 @@ class TacticPage extends StatefulWidget {
 class _TacticPageState extends State<TacticPage> {
   bool _isLoading = true;
   List<Player> footballers = [];
-  List<Player?> fieldPositions = List.filled(11, null);
+  Map<String, Player?> fieldPositions = {
+    'ST1': null,
+    'ST2': null,
+    'LM': null,
+    'CM1': null,
+    'CM2': null,
+    'RM': null,
+    'LB': null,
+    'CB1': null,
+    'CB2': null,
+    'RB': null,
+    'GK': null,
+  };
   String selectedFormation = '4-4-2';
 
   final Map<String, List<String>> formations = {
-    '4-4-2': ['ST', 'ST', 'LM', 'CM', 'CM', 'RM', 'LB', 'CB', 'CB', 'RB', 'GK'],
-    '4-3-3': ['ST', 'LW', 'RW', 'CM', 'CM', 'CM', 'LB', 'CB', 'CB', 'RB', 'GK'],
+    '4-4-2': [
+      'ST1',
+      'ST2',
+      'LM',
+      'CM1',
+      'CM2',
+      'RM',
+      'LB',
+      'CB1',
+      'CB2',
+      'RB',
+      'GK'
+    ],
+    '4-3-3': [
+      'ST1',
+      'LW',
+      'RW',
+      'CM1',
+      'CM2',
+      'CM3',
+      'LB',
+      'CB1',
+      'CB2',
+      'RB',
+      'GK'
+    ],
     '3-5-2': [
-      'ST',
-      'ST',
+      'ST1',
+      'ST2',
       'CAM',
       'LM',
-      'CM',
-      'CM',
+      'CM1',
+      'CM2',
       'RM',
-      'CB',
-      'CB',
-      'CB',
+      'CB1',
+      'CB2',
+      'CB3',
       'GK'
     ],
     // Add more formations as needed
@@ -79,7 +115,7 @@ class _TacticPageState extends State<TacticPage> {
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
                       IconButton(
-                        icon: Icon(Icons.arrow_left),
+                        icon: const Icon(Icons.arrow_left),
                         onPressed: () {
                           setState(() {
                             selectedFormation =
@@ -87,16 +123,19 @@ class _TacticPageState extends State<TacticPage> {
                           });
                         },
                       ),
-                      Text(
-                        selectedFormation,
-                        style: const TextStyle(
-                          color: AppColors.textEnabledColor,
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
+                      Expanded(
+                        child: Text(
+                          selectedFormation,
+                          textAlign: TextAlign.center,
+                          style: const TextStyle(
+                            color: AppColors.textEnabledColor,
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
                         ),
                       ),
                       IconButton(
-                        icon: Icon(Icons.arrow_right),
+                        icon: const Icon(Icons.arrow_right),
                         onPressed: () {
                           setState(() {
                             selectedFormation =
@@ -108,69 +147,8 @@ class _TacticPageState extends State<TacticPage> {
                   ),
                   Expanded(
                     flex: 3,
-                    child: GridView.builder(
-                      padding: const EdgeInsets.all(10),
-                      gridDelegate:
-                          const SliverGridDelegateWithFixedCrossAxisCount(
-                        crossAxisCount: 5,
-                        childAspectRatio: 0.8,
-                        mainAxisSpacing: 5,
-                        crossAxisSpacing: 5,
-                      ),
-                      itemCount: formations[selectedFormation]!.length,
-                      itemBuilder: (context, index) {
-                        return DragTarget<Player>(
-                          builder: (context, candidateData, rejectedData) {
-                            return Container(
-                              decoration: BoxDecoration(
-                                color: fieldPositions[index] != null
-                                    ? AppColors.green
-                                    : AppColors.hoverColor,
-                                border: Border.all(
-                                  color: AppColors.textEnabledColor,
-                                ),
-                                borderRadius: BorderRadius.circular(8),
-                              ),
-                              child: Stack(
-                                children: [
-                                  if (fieldPositions[index] == null)
-                                    Center(
-                                      child: Text(
-                                        formations[selectedFormation]![index],
-                                        style: const TextStyle(
-                                          color: AppColors.textEnabledColor,
-                                          fontWeight: FontWeight.bold,
-                                        ),
-                                      ),
-                                    ),
-                                  if (fieldPositions[index] != null)
-                                    Positioned.fill(
-                                      child: Draggable<Player>(
-                                        data: fieldPositions[index],
-                                        feedback: _buildPlayerAvatar(
-                                            fieldPositions[index]!),
-                                        childWhenDragging: Container(),
-                                        child: _buildPlayerAvatar(
-                                            fieldPositions[index]!),
-                                        onDragCompleted: () {
-                                          setState(() {
-                                            fieldPositions[index] = null;
-                                          });
-                                        },
-                                      ),
-                                    ),
-                                ],
-                              ),
-                            );
-                          },
-                          onAccept: (data) {
-                            setState(() {
-                              fieldPositions[index] = data;
-                              footballers.remove(data);
-                            });
-                          },
-                        );
-                      },
+                    child: Stack(
+                      children: _buildFieldPositions(),
                     ),
                   ),
                   const Divider(
@@ -218,6 +196,97 @@ class _TacticPageState extends State<TacticPage> {
     return keys[(currentIndex + 1) % keys.length];
   }
 
+  List<Widget> _buildFieldPositions() {
+    List<Widget> widgets = [];
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height * 0.5;
+
+    Map<String, Offset> positions = {
+      'ST1': Offset(width * 0.5 - 30, height * 0.1),
+      'ST2': Offset(width * 0.5 + 30, height * 0.1),
+      'LM': Offset(width * 0.1, height * 0.3),
+      'CM1': Offset(width * 0.3, height * 0.3),
+      'CM2': Offset(width * 0.7, height * 0.3),
+      'RM': Offset(width * 0.9, height * 0.3),
+      'LB': Offset(width * 0.1, height * 0.5),
+      'CB1': Offset(width * 0.3, height * 0.5),
+      'CB2': Offset(width * 0.7, height * 0.5),
+      'RB': Offset(width * 0.9, height * 0.5),
+      'GK': Offset(width * 0.5, height * 0.7),
+    };
+
+    List<String>? currentFormation = formations[selectedFormation];
+
+    if (currentFormation != null) {
+      for (var position in currentFormation) {
+        Offset? pos = positions[position];
+        if (pos != null) {
+          widgets.add(
+            Positioned(
+              left: pos.dx,
+              top: pos.dy,
+              child: DragTarget<Player>(
+                builder: (context, candidateData, rejectedData) {
+                  return Container(
+                    width: 60,
+                    height: 60,
+                    decoration: BoxDecoration(
+                      color: fieldPositions[position] != null
+                          ? AppColors.green
+                          : AppColors.hoverColor,
+                      border: Border.all(
+                        color: AppColors.textEnabledColor,
+                      ),
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    child: Stack(
+                      children: [
+                        if (fieldPositions[position] == null)
+                          Center(
+                            child: Text(
+                              position,
+                              style: const TextStyle(
+                                color: AppColors.textEnabledColor,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        if (fieldPositions[position] != null)
+                          Positioned.fill(
+                            child: Draggable<Player>(
+                              data: fieldPositions[position],
+                              feedback:
+                                  _buildPlayerAvatar(fieldPositions[position]!),
+                              childWhenDragging: Container(),
+                              child:
+                                  _buildPlayerAvatar(fieldPositions[position]!),
+                              onDragCompleted: () {
+                                setState(() {
+                                  fieldPositions[position] = null;
+                                });
+                              },
+                            ),
+                          ),
+                      ],
+                    ),
+                  );
+                },
+                onAccept: (data) {
+                  setState(() {
+                    fieldPositions[position] = data;
+                    footballers.remove(data);
+                  });
+                },
+              ),
+            ),
+          );
+        }
+      }
+    }
+
+    return widgets;
+  }
+
   Widget _buildPlayerAvatar(Player player) {
     return Column(
       mainAxisSize: MainAxisSize.min,
@@ -240,13 +309,16 @@ class _TacticPageState extends State<TacticPage> {
               ),
             ),
             const SizedBox(width: 4),
-            Text(
-              player.name,
-              style: const TextStyle(
-                color: AppColors.textEnabledColor,
-                fontSize: 14,
+            Flexible(
+              child: Text(
+                player.name,
+                style: const TextStyle(
+                  color: AppColors.textEnabledColor,
+                  fontSize: 14,
+                ),
+                textAlign: TextAlign.center,
+                overflow: TextOverflow.ellipsis,
               ),
-              textAlign: TextAlign.center,
             ),
           ],
         ),
