8cc9b68585adaea402292027fa86ff448b845fa1
Marcin Kawa <lerukcja2002@gmail.com>
2024-07-11 11:45:05 +0200

    BottomNavBar fix

--- lib/components/bottom_nav_bar.dart
+++ lib/components/bottom_nav_bar.dart
@@ -12,27 +12,36 @@ class BottomNavBar extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: navBarHeight,
+      width: MediaQuery.sizeOf(context).width,
       padding: const EdgeInsets.only(bottom: 0.0),
-      child: GNav(
-          onTabChange: (value) => onTabChange!(value),
-          mainAxisAlignment: MainAxisAlignment.center,
-          backgroundColor: AppColors.hoverColor,
-          tabBackgroundColor: AppColors.primaryColor,
-          activeColor: AppColors.textEnabledColor,
-          color: AppColors.textDisabledColor,
-          gap: 8,
-          tabs: const [
-            //home tam
-            GButton(icon: Icons.stadium, text: 'Stadium'),
-            // play tab
-            GButton(icon: Icons.play_circle_fill, text: 'Play'),
-            //transfer tab
-            GButton(icon: Icons.people_alt, text: 'Transfer'),
-            //league tab
-            GButton(icon: Icons.list_alt, text: 'League'),
-            //profile tab
-            GButton(icon: Icons.co_present_rounded, text: "Profile")
-          ]),
+      child: Row(
+        children: [
+          Expanded(
+            child: GNav(
+                onTabChange: (value) => onTabChange!(value),
+                mainAxisAlignment: MainAxisAlignment.center,
+                backgroundColor: AppColors.hoverColor,
+                tabBackgroundColor: AppColors.primaryColor,
+                activeColor: AppColors.textEnabledColor,
+                color: AppColors.textDisabledColor,
+                //gap: 8,
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+                tabs: const [
+                  //home tam
+                  GButton(icon: Icons.stadium, text: 'Stadium'),
+                  // play tab
+                  GButton(icon: Icons.play_circle_fill, text: 'Play'),
+                  //transfer tab
+                  GButton(icon: Icons.people_alt, text: 'Transfer'),
+                  //league tab
+                  GButton(icon: Icons.list_alt, text: 'League'),
+                  //profile tab
+                  GButton(icon: Icons.co_present_rounded, text: "Profile")
+                ]),
+          ),
+        ],
+      ),
     );
   }
 }
