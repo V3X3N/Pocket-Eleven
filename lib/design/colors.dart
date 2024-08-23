62ee55fd857e0b864e53415f361536d24a794bcc
Marcin Kawa <lerukcja2002@gmail.com>
2024-08-23 10:48:52 +0200

    get back old colors

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -2,9 +2,9 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   static const Color green = Color(0xFF028A0F);
-  static const Color primaryColor = Color(0xFF242834);
+  static const Color primaryColor = Color(0xFF2A2D3E);
   static const Color blueColor = Color(0xFF2697FF);
-  static const Color hoverColor = Color(0xFF181E2C);
+  static const Color hoverColor = Color(0xFF212332);
   static const Color buttonColor = Color(0xFF333645);
   static const Color textEnabledColor = Color(0xFFF8F5FA);
   static const Color coffeeText = Color(0xFFE1DCCE);
