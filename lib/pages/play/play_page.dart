2c074b53eb598095ca23444d4ee429823d754879
Marcin Kawa <lerukcja2002@gmail.com>
2024-08-23 11:38:50 +0200

    small play_page structure change

--- lib/pages/play/play_page.dart
+++ lib/pages/play/play_page.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:pocket_eleven/components/custom_appbar.dart';
 import 'package:pocket_eleven/components/option_button.dart';
 import 'package:pocket_eleven/design/colors.dart';
+import 'package:pocket_eleven/pages/play/widgets/match_container.dart';
+import 'package:pocket_eleven/pages/play/widgets/standings_container.dart';
 
 class PlayPage extends StatefulWidget {
   const PlayPage({super.key});
@@ -63,132 +65,14 @@ class _PlayPageState extends State<PlayPage> {
               child: IndexedStack(
                 index: _selectedIndex,
                 children: [
-                  _buildMatchContainer(screenWidth, screenHeight),
-                  _buildStandingsContainer(screenWidth, screenHeight),
+                  MatchContainer(
+                      screenWidth: screenWidth, screenHeight: screenHeight),
+                  StandingsContainer(
+                      screenWidth: screenWidth, screenHeight: screenHeight),
                 ],
               ),
             ),
           ],
-        ),
-      ),
-    );
-  }
-
-  Widget _buildMatchContainer(double screenWidth, double screenHeight) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.start,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        Align(
-          alignment: Alignment.topCenter,
-          child: Container(
-            // TODO: First Container for the next match
-            margin: EdgeInsets.all(screenWidth * 0.05),
-            padding: EdgeInsets.all(screenWidth * 0.04),
-            decoration: BoxDecoration(
-              color: AppColors.hoverColor,
-              border: Border.all(color: AppColors.borderColor, width: 1),
-              borderRadius: BorderRadius.circular(10.0),
-            ),
-            height: screenHeight * 0.25,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Container(
-                      height: screenWidth * 0.225,
-                      width: screenWidth * 0.225,
-                      decoration: BoxDecoration(
-                        borderRadius:
-                            BorderRadius.circular(screenWidth * 0.025),
-                        image: const DecorationImage(
-                          image: AssetImage(
-                              'assets/crests/crest_1.png'), // TODO: Display proper Players Club crest
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                    ),
-                    SizedBox(height: screenHeight * 0.01),
-                    const Text(
-                      "ClubName", // TODO: Display proper Players Club name
-                      style: TextStyle(
-                          color: AppColors.textEnabledColor, fontSize: 18),
-                    ),
-                  ],
-                ),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Container(
-                      height: screenWidth * 0.225,
-                      width: screenWidth * 0.225,
-                      decoration: BoxDecoration(
-                        borderRadius:
-                            BorderRadius.circular(screenWidth * 0.025),
-                        image: const DecorationImage(
-                          image: AssetImage(
-                              'assets/crests/crest_2.png'), // TODO: Display proper Players Club crest
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                    ),
-                    SizedBox(height: screenHeight * 0.01),
-                    const Text(
-                      'Klub 2', // TODO: Display proper Players Club name
-                      style: TextStyle(
-                          color: AppColors.textEnabledColor, fontSize: 18),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          ),
-        ),
-        Expanded(
-          child: Container(
-            // TODO: Second container for upcoming matches
-            margin: EdgeInsets.all(screenWidth * 0.05),
-            padding: EdgeInsets.all(screenWidth * 0.04),
-            decoration: BoxDecoration(
-              color: AppColors.hoverColor,
-              border: Border.all(color: AppColors.borderColor, width: 1),
-              borderRadius: BorderRadius.circular(10.0),
-            ),
-            child: const Center(
-              child: Text(
-                'Matches Container',
-                style: TextStyle(
-                  color: AppColors.textEnabledColor,
-                  fontSize: 18,
-                ),
-              ),
-            ),
-          ),
-        ),
-      ],
-    );
-  }
-
-  Widget _buildStandingsContainer(double screenWidth, double screenHeight) {
-    return Container(
-      // TODO: Container for league standings
-      margin: EdgeInsets.all(screenWidth * 0.04),
-      decoration: BoxDecoration(
-        color: AppColors.hoverColor,
-        border: Border.all(color: AppColors.borderColor, width: 1),
-        borderRadius: BorderRadius.circular(10.0),
-      ),
-      width: screenWidth,
-      height: screenHeight,
-      child: const Center(
-        child: Text(
-          'Standings Container',
-          style: TextStyle(
-            color: AppColors.textEnabledColor,
-            fontSize: 18,
-          ),
         ),
       ),
     );
