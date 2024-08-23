c3dde44178b3963078e0bd452f27734d25ae480a
Marcin Kawa <lerukcja2002@gmail.com>
2024-08-23 17:07:50 +0200

    Get data from firestore

--- lib/firebase/firebase_functions.dart
+++ lib/firebase/firebase_functions.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:pocket_eleven/models/player.dart';
 
 class FirebaseFunctions {
   static Future<void> saveUser(
@@ -115,6 +116,68 @@ class FirebaseFunctions {
     } catch (error) {
       debugPrint('Error loading club reference: $error');
       rethrow;
+    }
+  }
+
+  static Future<List<Player>> getPlayersForClub(String clubId) async {
+    try {
+      // Pobieranie dokumentu klubu
+      DocumentReference clubRef =
+          FirebaseFirestore.instance.doc('/clubs/$clubId');
+
+      // Pobieranie zawodników, którzy należą do tego klubu
+      QuerySnapshot snapshot = await FirebaseFirestore.instance
+          .collection('players')
+          .where('club', isEqualTo: clubRef)
+          .get();
+
+      // Mapowanie wyników na listę obiektów Player
+      return snapshot.docs.map((doc) {
+        Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
+        return Player(
+          name: data['name'],
+          position: data['position'],
+          ovr: data['ovr'],
+          age: data['age'],
+          nationality: data['nationality'],
+          imagePath: data['imagePath'],
+          flagPath: data['flagPath'],
+          value: data['value'],
+          salary: data['salary'],
+          param1: data['param1'],
+          param2: data['param2'],
+          param3: data['param3'],
+          param4: data['param4'],
+          param1Name: data['param1Name'],
+          param2Name: data['param2Name'],
+          param3Name: data['param3Name'],
+          param4Name: data['param4Name'],
+          matchesPlayed: data['matchesPlayed'],
+          goals: data['goals'],
+          assists: data['assists'],
+          yellowCards: data['yellowCards'],
+          redCards: data['redCards'],
+        );
+      }).toList();
+    } catch (error) {
+      debugPrint('Error fetching players: $error');
+      return [];
+    }
+  }
+
+  static Future<String> getClubId(String userId) async {
+    try {
+      DocumentSnapshot userDoc = await _getUserDocument(userId);
+      Map<String, dynamic>? userData = userDoc.data() as Map<String, dynamic>?;
+
+      if (userData != null && userData.containsKey('club')) {
+        DocumentReference clubRef = userData['club'];
+        return clubRef.id;
+      }
+      return '';
+    } catch (error) {
+      debugPrint('Error loading club id: $error');
+      return '';
     }
   }
 }
--- lib/pages/tactic/class/players_view.dart
+++ lib/pages/tactic/class/players_view.dart
@@ -1,8 +1,49 @@
 import 'package:flutter/material.dart';
 import 'package:pocket_eleven/design/colors.dart';
+import 'package:pocket_eleven/models/player.dart';
+import 'package:pocket_eleven/components/player_details.dart';
+import 'package:pocket_eleven/firebase/firebase_functions.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
-class PlayersView extends StatelessWidget {
+class PlayersView extends StatefulWidget {
   const PlayersView({super.key});
+
+  @override
+  _PlayersViewState createState() => _PlayersViewState();
+}
+
+class _PlayersViewState extends State<PlayersView> {
+  List<Player> players = [];
+  bool isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadPlayers();
+  }
+
+  Future<void> _loadPlayers() async {
+    final user = FirebaseAuth.instance.currentUser;
+    if (user != null) {
+      final String clubId = await FirebaseFunctions.getClubId(user.uid);
+      if (clubId.isNotEmpty) {
+        final List<Player> loadedPlayers =
+            await FirebaseFunctions.getPlayersForClub(clubId);
+        setState(() {
+          players = loadedPlayers;
+          isLoading = false;
+        });
+      } else {
+        setState(() {
+          isLoading = false;
+        });
+      }
+    } else {
+      setState(() {
+        isLoading = false;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,16 +59,43 @@ class PlayersView extends StatelessWidget {
       ),
       width: screenWidth,
       height: screenHeight,
-      child: const Center(
-        child: Text(
-          // TODO: Implement players data from firestore
-          'Players Container',
-          style: TextStyle(
-            color: AppColors.textEnabledColor,
-            fontSize: 18,
-          ),
-        ),
-      ),
+      child: isLoading
+          ? const Center(child: CircularProgressIndicator())
+          : players.isEmpty
+              ? const Center(
+                  child: Text(
+                    'No players found',
+                    style: TextStyle(
+                      color: AppColors.textEnabledColor,
+                      fontSize: 18,
+                    ),
+                  ),
+                )
+              : GridView.builder(
+                  padding: const EdgeInsets.all(8.0),
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 4,
+                    crossAxisSpacing: 8.0,
+                    mainAxisSpacing: 8.0,
+                  ),
+                  itemCount: players.length,
+                  itemBuilder: (context, index) {
+                    final player = players[index];
+                    return GestureDetector(
+                      onTap: () {
+                        showDialog(
+                          context: context,
+                          builder: (BuildContext context) {
+                            return PlayerDetailsDialog(player: player);
+                          },
+                        );
+                      },
+                      child: Container(
+                        color: Colors.green,
+                      ),
+                    );
+                  },
+                ),
     );
   }
 }
