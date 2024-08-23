c6d8452b208ef331064f001fb5be380d96f9f04b
Marcin Kawa <lerukcja2002@gmail.com>
2024-08-23 16:39:14 +0200

    add players to firestore

--- lib/pages/transfers/widgets/transfer_player_widget.dart
+++ lib/pages/transfers/widgets/transfer_player_widget.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:pocket_eleven/models/player.dart';
 import 'package:pocket_eleven/design/colors.dart';
 import 'package:pocket_eleven/components/player_details.dart';
+import 'package:pocket_eleven/firebase/firebase_functions.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class TransfersPlayerWidget extends StatelessWidget {
   final Player player;
@@ -10,6 +13,109 @@ class TransfersPlayerWidget extends StatelessWidget {
     super.key,
     required this.player,
   });
+
+  Future<void> _savePlayerToFirestore(BuildContext context) async {
+    final user = FirebaseAuth.instance.currentUser;
+    if (user == null) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('User not logged in')),
+      );
+      return;
+    }
+
+    final String userId = user.uid;
+    final String clubName = await FirebaseFunctions.getClubName(userId);
+
+    // Check if "players" collection exists
+    final playersCollection = FirebaseFirestore.instance.collection('players');
+    final snapshot = await playersCollection.limit(1).get();
+
+    if (snapshot.docs.isEmpty) {
+      // If no documents exist, create the collection by adding the first player
+      await playersCollection.add({
+        'name': player.name,
+        'position': player.position,
+        'ovr': player.ovr,
+        'age': player.age,
+        'nationality': player.nationality,
+        'imagePath': player.imagePath,
+        'flagPath': player.flagPath,
+        'value': player.value,
+        'salary': player.salary,
+        'param1': player.param1,
+        'param2': player.param2,
+        'param3': player.param3,
+        'param4': player.param4,
+        'param1Name': player.param1Name,
+        'param2Name': player.param2Name,
+        'param3Name': player.param3Name,
+        'param4Name': player.param4Name,
+        'matchesPlayed': player.matchesPlayed,
+        'goals': player.goals,
+        'assists': player.assists,
+        'yellowCards': player.yellowCards,
+        'redCards': player.redCards,
+        'club': clubName,
+      });
+    } else {
+      // If the collection exists, add the player to the existing collection
+      await playersCollection.add({
+        'name': player.name,
+        'position': player.position,
+        'ovr': player.ovr,
+        'age': player.age,
+        'nationality': player.nationality,
+        'imagePath': player.imagePath,
+        'flagPath': player.flagPath,
+        'value': player.value,
+        'salary': player.salary,
+        'param1': player.param1,
+        'param2': player.param2,
+        'param3': player.param3,
+        'param4': player.param4,
+        'param1Name': player.param1Name,
+        'param2Name': player.param2Name,
+        'param3Name': player.param3Name,
+        'param4Name': player.param4Name,
+        'matchesPlayed': player.matchesPlayed,
+        'goals': player.goals,
+        'assists': player.assists,
+        'yellowCards': player.yellowCards,
+        'redCards': player.redCards,
+        'club': clubName,
+      });
+    }
+
+    ScaffoldMessenger.of(context).showSnackBar(
+      const SnackBar(content: Text('Player added to your club successfully')),
+    );
+  }
+
+  Future<void> _confirmPlayerSelection(BuildContext context) async {
+    final bool? confirmed = await showDialog<bool>(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Confirm Selection'),
+          content: Text('Are you sure you want to select ${player.name}?'),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.of(context).pop(false),
+              child: const Text('Cancel'),
+            ),
+            TextButton(
+              onPressed: () => Navigator.of(context).pop(true),
+              child: const Text('Yes'),
+            ),
+          ],
+        );
+      },
+    );
+
+    if (confirmed == true) {
+      await _savePlayerToFirestore(context);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -74,7 +180,7 @@ class TransfersPlayerWidget extends StatelessWidget {
                 IconButton(
                   icon:
                       const Icon(Icons.check_box_rounded, color: Colors.green),
-                  onPressed: () {},
+                  onPressed: () => _confirmPlayerSelection(context),
                 ),
               ],
             ),
