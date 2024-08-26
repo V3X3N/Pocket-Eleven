a88bb6fd96e8526c9ee1da1c30bbb941fc2a6db4
Marcin Kawa <lerukcja2002@gmail.com>
2024-08-26 14:33:06 +0200

    custom_appbar displays user firestore currency

--- lib/components/custom_appbar.dart
+++ lib/components/custom_appbar.dart
@@ -1,18 +1,52 @@
 import 'package:flutter/material.dart';
-import 'package:pocket_eleven/managers/medical_manager.dart';
-import 'package:pocket_eleven/managers/training_manager.dart';
-import 'package:pocket_eleven/managers/user_manager.dart';
-import 'package:pocket_eleven/managers/youth_manager.dart';
+import 'package:pocket_eleven/firebase/firebase_functions.dart';
 import 'package:unicons/unicons.dart';
 import 'package:pocket_eleven/design/colors.dart';
 
-class ReusableAppBar extends StatelessWidget implements PreferredSizeWidget {
+class ReusableAppBar extends StatefulWidget implements PreferredSizeWidget {
   final double appBarHeight;
 
   const ReusableAppBar({
     super.key,
     required this.appBarHeight,
   });
+
+  @override
+  State<ReusableAppBar> createState() => _ReusableAppBarState();
+
+  @override
+  Size get preferredSize => Size.fromHeight(appBarHeight);
+}
+
+class _ReusableAppBarState extends State<ReusableAppBar> {
+  int stadiumPoints = 0;
+  int trainingPoints = 0;
+  int medicalPoints = 0;
+  int youthPoints = 0;
+  double money = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadUserData();
+  }
+
+  Future<void> _loadUserData() async {
+    try {
+      Map<String, dynamic> userData = await FirebaseFunctions.getUserData();
+      if (mounted) {
+        setState(() {
+          stadiumPoints = userData['stadiumPoints'] ?? 0;
+          trainingPoints = userData['trainingPoints'] ?? 0;
+          medicalPoints = userData['medicalPoints'] ?? 0;
+          youthPoints = userData['youthPoints'] ?? 0;
+          money = (userData['money'] ?? 0).toDouble();
+        });
+      }
+    } catch (e) {
+      debugPrint('Error loading user data: $e');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -27,14 +61,11 @@ class ReusableAppBar extends StatelessWidget implements PreferredSizeWidget {
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                _buildInfoRow(UniconsLine.no_entry,
-                    TrainingManager.trainingPoints.toString()),
-                _buildInfoRow(UniconsLine.medkit,
-                    MedicalManager.medicalPoints.toString()),
-                _buildInfoRow(
-                    UniconsLine.six_plus, YouthManager.youthPoints.toString()),
-                _buildInfoRow(UniconsLine.usd_circle,
-                    UserManager.money.toStringAsFixed(0)),
+                _buildInfoRow(UniconsLine.no_entry, stadiumPoints.toString()),
+                _buildInfoRow(UniconsLine.dumbbell, trainingPoints.toString()),
+                _buildInfoRow(UniconsLine.medkit, medicalPoints.toString()),
+                _buildInfoRow(UniconsLine.sixteen_plus, youthPoints.toString()),
+                _buildInfoRow(UniconsLine.usd_circle, money.toStringAsFixed(0)),
               ],
             ),
           ),
@@ -58,7 +89,4 @@ class ReusableAppBar extends StatelessWidget implements PreferredSizeWidget {
       ],
     );
   }
-
-  @override
-  Size get preferredSize => Size.fromHeight(appBarHeight);
 }
