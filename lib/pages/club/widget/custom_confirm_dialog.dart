b56fbcdb3c8277c9c14d1dc650123c46a2b5e701
Marcin Kawa <lerukcja2002@gmail.com>
2024-11-16 14:26:28 +0100

    add new dialog for stadium

---
+++ lib/pages/club/widget/custom_confirm_dialog.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:pocket_eleven/design/colors.dart';
+
+class CustomConfirmDialog extends StatelessWidget {
+  final String title;
+  final String message;
+  final VoidCallback onConfirm;
+  final VoidCallback onCancel;
+
+  const CustomConfirmDialog({
+    super.key,
+    required this.title,
+    required this.message,
+    required this.onConfirm,
+    required this.onCancel,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final double screenWidth = MediaQuery.of(context).size.width;
+    return Dialog(
+      child: ConstrainedBox(
+        constraints: BoxConstraints(maxWidth: screenWidth * 0.8),
+        child: Container(
+          padding: EdgeInsets.all(screenWidth * 0.04),
+          decoration: BoxDecoration(
+            color: AppColors.hoverColor,
+            border: Border.all(color: AppColors.borderColor, width: 1),
+            borderRadius: BorderRadius.circular(10.0),
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Text(
+                  title,
+                  style: const TextStyle(color: AppColors.textEnabledColor),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                child: Text(
+                  message,
+                  style: const TextStyle(color: AppColors.textEnabledColor),
+                ),
+              ),
+              const SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  TextButton(
+                    onPressed: () {
+                      Navigator.of(context).pop(false);
+                      onCancel();
+                    },
+                    child: const Text(
+                      'Cancel',
+                      style: TextStyle(color: Colors.red),
+                    ),
+                  ),
+                  const SizedBox(width: 8),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.of(context).pop(true);
+                      onConfirm();
+                    },
+                    child: const Text(
+                      'Upgrade',
+                      style: TextStyle(color: AppColors.blueColor),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
